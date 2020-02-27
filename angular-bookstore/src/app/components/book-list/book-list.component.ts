772e2e685cf5865effcfb50b2395f59e39f9a2ae
Umesh <umesh.chaudhary@gmail.com>
2020-02-27 11:13:20 +0900

    Completed feature 1

--- angular-bookstore/src/app/components/book-list/book-list.component.ts
+++ angular-bookstore/src/app/components/book-list/book-list.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Book } from '../../common/book';
+import { BookService } from '../../services/book.service';
 
 @Component({
   selector: 'app-book-list',
@@ -8,45 +9,20 @@ import { Book } from '../../common/book';
 })
 export class BookListComponent implements OnInit {
 
-  books: Book[] = [
-    {
-      sku: "text-100",
-      name: "C Programming Language",
-      description: "Learn C Programming Language",
-      unitPrice: 600,
-      imageUrl: "assets/images/books/text-100.png",
-      active: true,
-      unitsInStock: 100,
-      createdOn: new Date(),
-      updatedOn: null,
-    },
-    {
-      sku: "text-101",
-      name: "C# Crash Course",
-      description: "Learn C# Programming Language",
-      unitPrice: 900,
-      imageUrl: "assets/images/books/text-101.png",
-      active: true,
-      unitsInStock: 100,
-      createdOn: new Date(),
-      updatedOn: null,
-    },
-    {
-      sku: "text-102",
-      name: "C++ Crash Course",
-      description: "Learn C++ Programming Language",
-      unitPrice: 700,
-      imageUrl: "assets/images/books/text-102.png",
-      active: true,
-      unitsInStock: 100,
-      createdOn: new Date(),
-      updatedOn: null,
-    }
-  ]
+  books: Book[];
 
-  constructor() { }
+  constructor(private _bookService: BookService) { }
 
   ngOnInit(): void {
+    this.listBooks();
   }
 
+  listBooks(){
+    this._bookService.getBooks().subscribe(
+      data => this.books = data
+      // data => {
+      //   console.log(data);
+      // }
+    )
+  }
 }
